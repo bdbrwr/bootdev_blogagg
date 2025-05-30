083daf13e47d2dc6ea49ac6dcf4df389dc956a69
bdbrwr <bart@bdbrwr.nl>
2025-05-30 16:36:16 +0200

    Unfollow functionality

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -61,3 +61,26 @@ func printFeedFollow(username, feedname string) {
 	fmt.Printf("* User:          %s\n", username)
 	fmt.Printf("* Feed:          %s\n", feedname)
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %v <url>", cmd.Name)
+	}
+
+	url := cmd.Args[0]
+
+	feed, err := s.db.GetFeedByUrl(context.Background(), url)
+	if err != nil {
+		return err
+	}
+
+	err = s.db.RemoveFeedFollow(context.Background(), database.RemoveFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
